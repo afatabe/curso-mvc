2ca24c3c25d4ca953655ef4ebbed69de3dc93f9a
anderson f antonio <gilafa@hotmail.com>
2016-03-28 16:54:47 -0300

    Commit 3

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -9,10 +9,9 @@ namespace Quiron.LojaVirtual.Dominio.Entidades
     public class Produto
     {
         public int ProdutoId { get; set; }
+        public string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
         public string Categoria { get; set; }
-
-
     }
 }
